4919f3fe500c136ecc6cfd22ed055d09f1fb9bf0
TarlanSB <tarlanworkmail@gmail.com>
2025-03-18 14:11:48 +0300

    initial commit starter

--- src/main/java/ru/tsb/loggingstarter/LoggingStarterAutoConfiguration.java
+++ src/main/java/ru/tsb/loggingstarter/LoggingStarterAutoConfiguration.java
@@ -1,8 +1,31 @@
 package ru.tsb.loggingstarter;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import ru.tsb.loggingstarter.aspect.LogExecutionAspect;
+import ru.tsb.loggingstarter.webfilter.WebLoggingFilter;
+import ru.tsb.loggingstarter.webfilter.WebLoggingRequestBodyAdvice;
+
+@AutoConfiguration
+@ConditionalOnProperty(prefix = "logging", value = "enabled", havingValue = "true", matchIfMissing = true)
 public class LoggingStarterAutoConfiguration {
 
-    public static void println(String str) {
-        System.out.println("Вызван из gradle библиотеки: " + str);
+    @Bean
+    @ConditionalOnProperty(prefix = "logging", value = "log-exec-time", havingValue = "true")
+    public LogExecutionAspect logExecutionAspect() {
+        return  new LogExecutionAspect();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "logging.web-logging", value = "enabled", havingValue = "true", matchIfMissing = true)
+    public WebLoggingFilter webLoggingFilter(){
+        return new WebLoggingFilter();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "logging.web-logging", value = {"enabled", "log-body"}, havingValue = "true", matchIfMissing = true)
+    public WebLoggingRequestBodyAdvice webLoggingRequestBodyAdvice(){
+        return new WebLoggingRequestBodyAdvice();
     }
 }
